46c2da6029fe5467e3be5510bfa6261d3bbdc64c
MelaBrtn <bertinmelanie.pro@gmail.com>
2022-09-21 17:01:48 +0200

    Création Page vérification + Cnx BDD

---
+++ verification.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <!-- Connexion BDD-->
+        <?php   
+        $bdd = new PDO('mysql:host=127.0.0.1;dbname=tpslam3versioning-mb', 'root', '');
+
+        $recupUsername = $_POST['username'];
+        $recupPassword = $_POST['password'];
+
+        $req = $bdd->prepare('SELECT Login, MotDePasse FROM utilisateur');
+        $req->execute(array(
+            'username' => $recupUsername,
+            'password' => $recupPassword));
+        
+        $resultat = $req->fetch();
+        
+        if (!$req)
+        {
+            header('Location: inscrire.php');
+        }
+        elseif(!req)
+        {
+        
+            echo 'Vous êtes connecté !';
+        }
