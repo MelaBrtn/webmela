85fec6d1414f98066e1018d29e0a8bd9b5bf5061
MelaBrtn <bertinmelanie.pro@gmail.com>
2022-09-28 16:42:10 +0200

    commit

--- confirmation.php
+++ confirmation.php
@@ -1,5 +1,7 @@
 <html>
-    r le fichier de style -->
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
         <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
     </head>
     <body>
