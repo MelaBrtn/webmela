5646db6877759e82f3c452b3fba85b0da110dbb3
MelaBrtn <bertinmelanie.pro@gmail.com>
2022-09-28 17:00:20 +0200

    Création inscrire.php

---
+++ inscrire.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <form name="inscription" method="post" action="">
+            <label>Pseudo</label>
+            <input type="text" placeholder="Mettre votre pseudo" name="pseudo" required>
+            <label>Mot de passe</label>
+            <input type="password" placeholder="Entre votre mot de passe" name="password" required>
+            <label>Retapez votre mot de passe</label>
+            <input type="password" name ="password" required>
+            <label>Adresse email</label>
+            <input type="mail" placeholder="Entrez votre email" name="mail" required>
+</form>
+
+</body>
