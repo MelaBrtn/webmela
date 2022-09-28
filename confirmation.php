915ddc00ab572baf0416d29d9ed13edc79121c53
MelaBrtn <bertinmelanie.pro@gmail.com>
2022-09-28 16:34:41 +0200

    Création confirmation.php

---
+++ confirmation.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <h1> Si vous voyez ce message c'est que vous êtes connecté<h1>
+        </div>
+    </body>
+</html>
