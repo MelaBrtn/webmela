09d34dd5355f5f74dca103c6537e2edbb13ba920
MelaBrtn <bertinmelanie.pro@gmail.com>
2022-10-19 16:38:54 +0200

    Modification verification.php pour password_verify

--- verification.php
+++ verification.php
@@ -3,26 +3,54 @@
 
         if (isset($_POST['submitLogin'])) {
          if (empty($_POST['username'])) {
-             $errors['username'] = 'Username or email required';
+             $errors['username'] = 'Le login est nécessaire';
          }
          if (empty($_POST['password'])) {
-             $errors['password'] = 'Password required';
+             $errors['password'] = 'Le mot de passe est nécessaire';
             }
          }
 
         $recupUsername = $_POST['username'];
         $recupPassword = $_POST['password'];
 
-        $stmt = $bdd->prepare('SELECT * FROM utilisateur WHERE Login = ? LIMIT 1;');
-        $stmt->execute([$recupUsername]);
-        $recupUsername = $stmt->fetch(\PDO::FETCH_ASSOC);
+        if (count($errors) === 0) {
+         $query = "SELECT * FROM utilisateur WHERE Login = ? LIMIT 1";
+         $stmt = $bdd->prepare($query);
+         $stmt->bind_param($recupUsername, $recupPassword);
 
-        if ($recupPassword == $recupUsername['MotDePasse']) {
-           return header('location: confirmation.php');
-        } 
-        else {
-           return header('Location: inscrire.php');
+         if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            $recupUsername = $result->fetch_assoc();
+            if (password_verify($recupPassword, $recupUsername['password'])) { // if password matches
+                $stmt->close();
 
-
+                $_SESSION['id'] = $recupUsername['id'];
+                $_SESSION['username'] = $recupUsername['username'];
+                $_SESSION['email'] = $recupUsername['email'];
+                $_SESSION['verified'] = $recupUsername['verified'];
+                $_SESSION['message'] = 'Vous êtes connecté';
+                $_SESSION['type'] = 'alert-success';
+                header('location: confirmation.php');
+                exit(0);
+            } else { // if password does not match
+                $errors['login_fail'] = "Mauvais utilisateur ou mot de passe saisit";
             }
+        } else {
+            $_SESSION['message'] = "L'identification a échoué";
+            $_SESSION['type'] = "alert-danger";
+        }
+    }
+
+
+      //  $stmt = $bdd->prepare('SELECT * FROM utilisateur WHERE Login = ? LIMIT 1;');
+       // $stmt->execute([$recupUsername]);
+       // $recupUsername = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+    //    if ($recupPassword == $recupUsername['MotDePasse']) {
+      //     return header('location: confirmation.php');
+        //} 
+       // else {
+         //  return header('Location: inscrire.php');
+
+           // }
 ?>
